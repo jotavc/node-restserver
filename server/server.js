174f9cb5003a6e55dbfd50d978046827f8b0dda9
Josu <condevall@gmail.com>
2018-12-12 14:43:41 +0100

    ResServer -usuario

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -11,46 +13,21 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
-});
 
-app.post('/usuario', function(req, res) {
+app.use(require('./routes/usuario'));
 
-    let body = req.body;
+//Establecer conexion a la base de datos
+mongoose.connect(process.env.URLDB, (err, res) => {
 
-    if (body.nombre === undefined) {
+    if (err) throw err;
 
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-
-    } else {
-
-        res.json({
-            persona: body
-        });
-
-    }
-
+    console.log('Base de datos ONLINE');
 
 });
 
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    })
-
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
-});
 
 app.listen(process.env.PORT, () => {
     console.log(`Escuchando puerto ${ process.env.PORT }`);
 });
+
+//v10.13.0
